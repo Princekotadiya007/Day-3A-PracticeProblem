9010b8baed09ba19e2705f75e91b25b2ba27c884
Princekotadiya007 <princuuu77@gmail.com>
2023-04-14 18:14:21 +0530

    program ploymorphism

--- Day-3A-PracticeProblem/Day-3A-PracticeProblem/Program.cs
+++ Day-3A-PracticeProblem/Day-3A-PracticeProblem/Program.cs
@@ -10,12 +10,41 @@ namespace Day_3A_PracticeProblem
     {
         static void Main(string[] args)
         {
-            ClassOfObject person1 = new ClassOfObject();
+            bool flag = true;
+            while (flag)
+            {
 
-            person1.Name = "prince";
-            person1.Age = 23;
+                Console.WriteLine(" Plase enter the option");
+                Console.WriteLine("Select Option 1");
+                Console.WriteLine("Select Option 2");
+                Console.WriteLine("Select Option 3");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
 
-            person1.SayHello();
+                        ClassOfObject person1 = new ClassOfObject(); // Creating object
+
+                        person1.Name = "prince";
+                        person1.Age = 23;
+
+                        person1.SayHello();
+                        break;
+
+                    case 2:
+                        Dog dog = new Dog();
+                        dog.Eat();
+                        dog.Bark();
+                        break;
+                    case 3:
+                        Polymorphism obj = new Polymorphism();
+                        obj.Add(10, 20);
+                        obj.Add(10.5f, 20.5f);
+                        obj.Add("prince", "kotadiya");
+                        Console.ReadLine();
+                        break;
+                }
+            }
         }
     }
 }
