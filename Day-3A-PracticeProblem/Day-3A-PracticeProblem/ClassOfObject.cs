8c8ec85395c83b6cd8607891cb23cbacc53c4609
Princekotadiya007 <princuuu77@gmail.com>
2023-04-14 18:00:17 +0530

    create class of object

---
+++ Day-3A-PracticeProblem/Day-3A-PracticeProblem/ClassOfObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3A_PracticeProblem
+{
+    internal class ClassOfObject
+    {
+        public string Name;
+        public int Age;
+        // Class methods
+        public void SayHello()
+        {
+            Console.WriteLine("Hello, my name is " + Name + " and I am " + Age + " years old.");
+        }
+    }
+}
---
+++ Day-3A-PracticeProblem/Day-3A-PracticeProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3A_PracticeProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassOfObject person1 = new ClassOfObject();
+
+            person1.Name = "prince";
+            person1.Age = 23;
+
+            person1.SayHello();
+        }
+    }
+}
